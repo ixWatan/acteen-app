dd436829316848ae2e31c768183f726ccf32ea4c
Watan Asaad <70540865+ixWatan@users.noreply.github.com>
2023-04-28 09:25:09 +0300

    aaaaa

---
+++ app/src/main/java/com/example/meet_workshop/NewProfile.java
@@ -0,0 +1,14 @@
+package com.example.meet_workshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_profile);
+    }
+}
