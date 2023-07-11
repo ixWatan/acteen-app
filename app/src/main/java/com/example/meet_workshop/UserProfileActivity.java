8b7c3dc48c8417b776b5e0f4460af2b20b16ba3e
Watan Asaad <70540865+ixWatan@users.noreply.github.com>
2023-07-12 01:33:17 +0300

    Sign Out Bugging and Glitching

--- app/src/main/java/com/example/meet_workshop/UserProfileActivity.java
+++ app/src/main/java/com/example/meet_workshop/UserProfileActivity.java
@@ -5,21 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class UserProfileActivity extends AppCompatActivity {
+
+    private Button signOutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        signOutBtn = findViewById(R.id.button3);
     }
 
-    public void goToHomePageAndSignOut(View view) {
-        FirebaseAuth.getInstance().signOut();
-        Intent intent = new Intent(UserProfileActivity.this, MainActivity.class);
+
+
+    public void signOut() {
+        signOutBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(UserProfileActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+    public void goToOrgOrActActivity(View view) {
+        Intent intent = new Intent(this, OrgOrActActivity.class);
         startActivity(intent);
     }
+
+    /*
+    protected void goToHomePageAndSignOut(View view) {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+     */
 }
