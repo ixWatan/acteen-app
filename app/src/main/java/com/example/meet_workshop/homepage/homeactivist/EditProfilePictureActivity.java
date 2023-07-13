9d75ae17f82ae3e93775e088a69c87ab9749956c
Student <nevotap1234@gmail.com>
2023-07-13 20:09:09 +0300

    Nevo Updates

--- app/src/main/java/com/example/meet_workshop/homepage/homeactivist/EditProfilePictureActivity.java
+++ app/src/main/java/com/example/meet_workshop/homepage/homeactivist/EditProfilePictureActivity.java
@@ -1,7 +1,13 @@
 package com.example.meet_workshop.homepage.homeactivist;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -11,6 +17,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
 import com.bumptech.glide.Glide;
 import com.example.meet_workshop.R;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -18,15 +25,20 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class EditProfilePictureActivity extends AppCompatActivity {
     private static final int PICK_IMAGE_REQUEST_CODE = 1;
     private static final int CAMERA_REQUEST_CODE = 2;
-    private FirebaseAuth mAuth;
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 3;
 
+    private FirebaseAuth mAuth;
     private ImageView profileImageView;
+    private Button selectImageButton;
+    private Button cameraButton;
     private Button saveButton;
 
     private Uri selectedImageUri;
@@ -38,20 +50,27 @@ public class EditProfilePictureActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
 
         profileImageView = findViewById(R.id.profile_image_view);
+        selectImageButton = findViewById(R.id.select_image_button);
+        cameraButton = findViewById(R.id.camera_button);
         saveButton = findViewById(R.id.save_button);
 
-        // Retrieve the current profile picture URL or file path from the intent extras
         String currentProfilePictureUrl = getIntent().getStringExtra("profilePictureUrl");
 
-        // Display the current profile picture in the ImageView
         if (currentProfilePictureUrl != null && !currentProfilePictureUrl.isEmpty()) {
             Glide.with(this).load(currentProfilePictureUrl).into(profileImageView);
         }
 
-        profileImageView.setOnClickListener(new View.OnClickListener() {
+        selectImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 selectImage();
+            }
+        });
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestCameraPermission();
             }
         });
 
@@ -59,11 +78,9 @@ public class EditProfilePictureActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (selectedImageUri != null) {
-                    // Process and save the new profile picture
                     String newProfilePictureUrl = selectedImageUri.toString();
                     updateProfilePicture(newProfilePictureUrl);
                 } else {
-                    // No image selected
                     Toast.makeText(EditProfilePictureActivity.this, "No image selected", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -71,34 +88,24 @@ public class EditProfilePictureActivity extends AppCompatActivity {
     }
 
     private void selectImage() {
-        // Create an intent to capture a new image using the camera
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Start the camera activity and retrieve the captured image
-            startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
-        }
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_IMAGE_REQUEST_CODE);
     }
 
-
     private void updateProfilePicture(String profilePictureUrl) {
-        // Perform the necessary logic to update the user's profile picture
-        // This may involve uploading the image to a storage service, updating the user's profile data, etc.
-
         FirebaseUser user = mAuth.getCurrentUser();
         if (user != null) {
-            String userId = user.getUid(); // Replace with the actual user ID
+            String userId = user.getUid();
             FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-            // Update the profile picture URL in the Firestore document
             db.collection("teenActivists").document(userId)
                     .update("profilePictureUrl", profilePictureUrl)
                     .addOnSuccessListener(new OnSuccessListener<Void>() {
                         @Override
                         public void onSuccess(Void aVoid) {
-                            // Profile picture update successful
                             Toast.makeText(EditProfilePictureActivity.this, "Profile picture updated successfully", Toast.LENGTH_SHORT).show();
 
-                            // Once the update is complete, navigate back to the UserProfileActivity or the relevant activity
                             Intent intent = new Intent();
                             intent.putExtra("profilePictureUrl", profilePictureUrl);
                             setResult(RESULT_OK, intent);
@@ -108,7 +115,6 @@ public class EditProfilePictureActivity extends AppCompatActivity {
                     .addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception e) {
-                            // Profile picture update failed
                             Toast.makeText(EditProfilePictureActivity.this, "Failed to update profile picture. Please try again.", Toast.LENGTH_SHORT).show();
                         }
                     });
@@ -121,55 +127,51 @@ public class EditProfilePictureActivity extends AppCompatActivity {
 
         if (requestCode == PICK_IMAGE_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
             selectedImageUri = data.getData();
-            // Perform image manipulation, such as resizing
-            Bitmap resizedBitmap = resizeImage(selectedImageUri);
 
-            // Display the resized image in the profileImageView
-            profileImageView.setImageBitmap(resizedBitmap);
-        } else if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
-            // Retrieve the captured image from the intent
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-
-            // Perform image manipulation, such as resizing
-            Bitmap resizedBitmap = resizeImage(imageBitmap);
-
-            // Display the resized image in the profileImageView
-            profileImageView.setImageBitmap(resizedBitmap);
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImageUri);
+                profileImageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+            Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
+            selectedImageUri = getImageUri(imageBitmap);
+            profileImageView.setImageBitmap(imageBitmap);
         }
     }
 
-    private Bitmap resizeImage(Uri imageUri) {
-        try {
-            // Load the image from the URI using InputStream and decode it into a Bitmap
-            InputStream inputStream = getContentResolver().openInputStream(imageUri);
-            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+    private Uri getImageUri(Bitmap bitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "Title", null);
+        return Uri.parse(path);
+    }
 
-            // Resize the image to the desired dimensions (e.g., 500x500 pixels)
-            int desiredWidth = 500;
-            int desiredHeight = 500;
-            Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, desiredWidth, desiredHeight, false);
 
-            // Clean up resources
-            inputStream.close();
-            bitmap.recycle();
-
-            return resizedBitmap;
-        } catch (IOException e) {
-            e.printStackTrace();
+    private void requestCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+        } else {
+            startCameraActivity();
         }
-
-        return null;
     }
 
-    private Bitmap resizeImage(Bitmap imageBitmap) {
-        // Resize the image to the desired dimensions (e.g., 500x500 pixels)
-        int desiredWidth = 500;
-        int desiredHeight = 500;
-        Bitmap resizedBitmap = Bitmap.createScaledBitmap(imageBitmap, desiredWidth, desiredHeight, false);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        return resizedBitmap;
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startCameraActivity();
+            } else {
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
-
+    private void startCameraActivity() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_REQUEST_CODE);
+    }
 }
