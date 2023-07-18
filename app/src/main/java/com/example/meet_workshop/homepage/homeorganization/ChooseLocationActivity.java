1e25b9d1580e19654a7eef0d29e8249956b19c7d
NevoTapiero <94896253+NevoTapiero@users.noreply.github.com>
2023-07-18 23:16:57 +0300

    Revert "habibi"
    This reverts commit ed74c154423ebc9147ca3621ffc6de57efb14ddd.

--- app/src/main/java/com/example/meet_workshop/homepage/homeorganization/ChooseLocationActivity.java
+++ app/src/main/java/com/example/meet_workshop/homepage/homeorganization/ChooseLocationActivity.java
@@ -3,20 +3,63 @@ package com.example.meet_workshop.homepage.homeorganization;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.meet_workshop.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.libraries.places.api.model.Place;
+
+import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.Toast;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+import com.google.android.libraries.places.api.model.Place;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import com.example.meet_workshop.R;
-import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -27,43 +70,36 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.net.PlacesClient;
-import com.google.android.libraries.places.widget.Autocomplete;
-import com.google.android.libraries.places.widget.AutocompleteActivity;
-import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class ChooseLocationActivity extends AppCompatActivity implements OnMapReadyCallback,
         GoogleMap.OnMapClickListener, GoogleMap.OnMarkerDragListener {
 
-    private static final int AUTOCOMPLETE_REQUEST_CODE = 1;
-    private PlacesClient placesClient;
 
     private LatLng selectedLocation;
 
+    private AutoCompleteTextView placeAutoCompleteTextView;
+    private Button searchButton;
+    private static final int AUTOCOMPLETE_REQUEST_CODE = 1;
+    private PlacesClient placesClient;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 100;
-    private EditText locationSearchEditText;
 
     private GoogleMap mMap;
     private Marker selectedMarker;
+    private Button confirmButton;
     private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_location);
-        locationSearchEditText = findViewById(R.id.locationBtn);
-        locationSearchEditText.setOnEditorActionListener((v, actionId, event) -> {
-            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                performLocationSearch();
-                return true;
-            }
-            return false;
-        });
+
+
+        ChooseLocationActivity chooseLocationActivity = new ChooseLocationActivity();
+        LatLng selectedLocation = chooseLocationActivity.getSelectedLocation();
 
         // Initialize the Places API
         Places.initialize(getApplicationContext(), "AIzaSyD48qyP0H6YM9-TezNQYys-n6i5TUgf3pY");
@@ -71,28 +107,40 @@ public class ChooseLocationActivity extends AppCompatActivity implements OnMapRe
         // Create a PlacesClient instance
         placesClient = Places.createClient(this);
 
+        confirmButton = findViewById(R.id.confirmButton);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                confirmLocation();
+            }
+        });
+        placeAutoCompleteTextView = findViewById(R.id.placeAutoCompleteTextView);
+        searchButton = findViewById(R.id.searchButton);
 
-
-
-
-
-
+        // Set click listener for the Search button
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Launch the autocomplete activity
+                List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG);
+                Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fields)
+                        .build(ChooseLocationActivity.this);
+                startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
+            }
+        });
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
+        // Check location permission and initialize the map if granted
         if (checkLocationPermission()) {
             initializeMap();
         } else {
             requestLocationPermission();
         }
-
-
     }
 
     private boolean checkLocationPermission() {
-        int permissionState = PackageManager.PERMISSION_GRANTED;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
-            permissionState = checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION);
-        }
+        int permissionState = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
         return permissionState == PackageManager.PERMISSION_GRANTED;
     }
 
@@ -114,6 +162,7 @@ public class ChooseLocationActivity extends AppCompatActivity implements OnMapRe
         mMap.setOnMapClickListener(this);
         mMap.setOnMarkerDragListener(this);
 
+        // Enable the My Location button
         if (checkLocationPermission()) {
             mMap.setMyLocationEnabled(true);
             fusedLocationClient.getLastLocation().addOnSuccessListener(this, location -> {
@@ -131,9 +180,13 @@ public class ChooseLocationActivity extends AppCompatActivity implements OnMapRe
             selectedMarker.remove();
         }
 
+        // Add a marker at the clicked location and enable dragging
         selectedMarker = mMap.addMarker(new MarkerOptions()
                 .position(latLng)
                 .draggable(true));
+
+        // Show the confirm button
+        confirmButton.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -154,9 +207,15 @@ public class ChooseLocationActivity extends AppCompatActivity implements OnMapRe
     private void confirmLocation() {
         if (selectedMarker != null) {
             selectedLocation = selectedMarker.getPosition();
+            // Handle the confirmed location
             String locationString = "Selected Location: " + selectedLocation.latitude + ", " + selectedLocation.longitude;
             Toast.makeText(this, locationString, Toast.LENGTH_SHORT).show();
         }
+    }
+
+
+    public LatLng getSelectedLocation() {
+        return selectedLocation;
     }
 
     @Override
@@ -181,11 +240,8 @@ public class ChooseLocationActivity extends AppCompatActivity implements OnMapRe
                 if (place != null) {
                     // Handle the selected place
                     LatLng selectedLocation = place.getLatLng();
-                    // Update the map and marker accordingly
+                    // Update the map and marker
                     // ...
-
-                    // Example: Move the camera to the selected location
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(selectedLocation, 15f));
                 }
             } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
                 Status status = Autocomplete.getStatusFromIntent(data);
@@ -195,22 +251,4 @@ public class ChooseLocationActivity extends AppCompatActivity implements OnMapRe
             }
         }
     }
-
-
-    private void performLocationSearch() {
-        String searchQuery = locationSearchEditText.getText().toString().trim();
-
-        // Set up the fields to be retrieved for the autocomplete results
-        List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG);
-
-        // Set up the intent for the autocomplete activity
-        Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fields)
-                .setCountries(Collections.singletonList("YOUR_COUNTRY_CODE"))
-                .build(this);
-
-        // Start the autocomplete activity
-        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
-    }
-
-
 }
