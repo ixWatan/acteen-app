d2d641f4d4ce754200b230718da2fd3331fe9902
Watan Asaad <70540865+ixWatan@users.noreply.github.com>
2023-07-24 17:00:51 +0300

    changes

--- app/src/main/java/com/example/meet_workshop/OnboardingFragment4.java
+++ app/src/main/java/com/example/meet_workshop/OnboardingFragment4.java
@@ -7,17 +7,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link OnboardingFragment4#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class OnboardingFragment4 extends Fragment {
+
+public class OnboardingFragment4 extends Fragment{
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
+
+    Button nextBtn;
+
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
@@ -40,6 +45,8 @@ public class OnboardingFragment4 extends Fragment {
     public static OnboardingFragment4 newInstance(String param1, String param2) {
         OnboardingFragment4 fragment = new OnboardingFragment4();
         Bundle args = new Bundle();
+
+
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
@@ -49,6 +56,7 @@ public class OnboardingFragment4 extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
--- app/src/main/java/com/example/meet_workshop/homepage/homeactivist/SearchActivityActivist.java
+++ app/src/main/java/com/example/meet_workshop/homepage/homeactivist/SearchActivityActivist.java
@@ -1,11 +1,13 @@
 package com.example.meet_workshop.homepage.homeactivist;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -16,15 +18,26 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.meet_workshop.MainActivity;
 import com.example.meet_workshop.R;
+import com.example.meet_workshop.ShowPostActivity;
+import com.example.meet_workshop.homepage.adapters.AdapterPosts;
+import com.example.meet_workshop.homepage.interfaces.SelectListener;
+import com.example.meet_workshop.homepage.models.ModelPost;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
@@ -34,10 +47,19 @@ import com.google.firebase.storage.UploadTask;
 import org.w3c.dom.Text;
 
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
-public class SearchActivityActivist extends AppCompatActivity {
+public class SearchActivityActivist extends AppCompatActivity implements SelectListener {
 
     private FirebaseAuth mAuth;
+
+    RecyclerView recyclerView;
+    List<ModelPost> postList;
+    AdapterPosts adapterPosts;
+
+    View catg;
 
     private ImageButton notificationButton;
     private ImageButton searchButton;
@@ -55,10 +77,11 @@ public class SearchActivityActivist extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_activist);
         mAuth = FirebaseAuth.getInstance();
-       /* search = findViewById(R.id.simpleSearchView);
+        search = findViewById(R.id.simpleSearchView);
 
-        search.setQueryHint("Search");
-*/
+        catg = findViewById(R.id.all_categories);
+
+
 
 
         profileImageView = findViewById(R.id.profileImageView);
@@ -69,6 +92,31 @@ public class SearchActivityActivist extends AppCompatActivity {
 
         ImageButton NavButton = (ImageButton) this.findViewById(R.id.nav_searchActivist);
         NavButton.setColorFilter(Color.rgb(0,0,0)); // Yellow Tint
+
+
+        // post recycler view properties
+        recyclerView = findViewById(R.id.postRecyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        //show newest first
+        layoutManager.setStackFromEnd(true);
+        layoutManager.setReverseLayout(true);
+
+        // set layout to recyclerview
+        recyclerView.setLayoutManager(layoutManager);
+
+        ProgressDialog pd;
+
+
+
+
+        // init post list
+        postList = new ArrayList<>();
+
+        // init the adapter
+        adapterPosts = new AdapterPosts(this, postList, this);
+
+        // set the adapter to recyclerview
+        recyclerView.setAdapter(adapterPosts);
 
 
 
@@ -155,11 +203,116 @@ public class SearchActivityActivist extends AppCompatActivity {
                     .into(profileImageView);
         }
 
+        //serach view listener
+
+        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+
+                //user pressed on search button
+                if(!TextUtils.isEmpty(s)) {
+                    searchPosts(s);
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+
+                //user presses any button
+                return false;
+            }
+        });
+
 
     }
 
+    private void searchPosts(String searchQuery) {
+        //path of  all posts
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Posts");
+
+        catg.setVisibility(View.GONE);
 
 
+
+        // get all data from this ref
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                postList.clear();
+                for(DataSnapshot ds: dataSnapshot.getChildren()) {
+                    ModelPost modelPost = ds.getValue(ModelPost.class);
+
+                    if(modelPost.getpTitle().toLowerCase().contains(searchQuery.toLowerCase())
+                            || modelPost.getpDescription().toLowerCase().contains(searchQuery.toLowerCase())
+                    ){
+                        postList.add(modelPost);
+                    }
+
+                    // adapter
+                    adapterPosts = new AdapterPosts(SearchActivityActivist.this, postList, SearchActivityActivist.this);
+
+                    //set  adapter to recyclerview
+                    recyclerView.setAdapter(adapterPosts);
+
+                }
+
+                adapterPosts.notifyDataSetChanged();
+
+
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // in case of error
+                Toast.makeText(SearchActivityActivist.this, "" + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+
+    private void loadPosts() {
+        //path of  all posts
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Posts");
+
+        // get all data from this ref
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                postList.clear();
+                for(DataSnapshot ds: dataSnapshot.getChildren()) {
+                    ModelPost modelPost = ds.getValue(ModelPost.class);
+
+                    postList.add(modelPost);
+
+
+                    // adapter
+                    adapterPosts = new AdapterPosts(SearchActivityActivist.this, postList, SearchActivityActivist.this);
+
+                    //set  adapter to recyclerview
+                    recyclerView.setAdapter(adapterPosts);
+
+                }
+
+                adapterPosts.notifyDataSetChanged();
+
+
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // in case of error
+                Toast.makeText(SearchActivityActivist.this, "" + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
 
 
 
@@ -182,6 +335,32 @@ public class SearchActivityActivist extends AppCompatActivity {
 
     private void openSearch() {
         Intent intent = new Intent(this, SearchActivityActivist.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onItemClicked(ModelPost modelPost) {
+        // Handle the item click event
+
+        Intent intent = new Intent(this, ShowPostActivity.class);
+        intent.putExtra("org_name",  modelPost.getuName());
+        intent.putExtra("post_image",  modelPost.getpImage());
+        intent.putExtra("post_description",  modelPost.getpDescription());
+        intent.putExtra("post_timePosted",  modelPost.getpTime());
+        intent.putExtra("post_title",  modelPost.getpTitle());
+        intent.putExtra("post_user_pfp",  modelPost.getuDp());
+        intent.putExtra("post_endT", modelPost.getpEndT());
+        intent.putExtra("post_startT", modelPost.getpStartT());
+        intent.putExtra("post_date", modelPost.getpDate());
+        intent.putExtra("post_tags", modelPost.getpHashtags());
+        intent.putExtra("post_locationLink", modelPost.getpLocationLink());
+        intent.putExtra("post_location", modelPost.getpLocationLinkReal());
+
+
+
+
+
+
         startActivity(intent);
     }
 
