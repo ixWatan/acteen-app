1cf54504883df29f5ad93373c0e386c825180f75
Watan Asaad <70540865+ixWatan@users.noreply.github.com>
2023-07-26 17:02:03 +0300

    auto-login feature

--- app/src/main/java/com/example/meet_workshop/StartEveryThing.java
+++ app/src/main/java/com/example/meet_workshop/StartEveryThing.java
@@ -6,9 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.meet_workshop.SlideShowAct.SlideShowActivityAct1;
 import com.example.meet_workshop.SlideShowOrg.SlideShowActivityOrg1;
+import com.example.meet_workshop.homepage.homeactivist.HomeActivity;
+import com.example.meet_workshop.homepage.homeorganization.CampaignManagementOrgActivity;
+import com.example.meet_workshop.homepage.homeorganization.HomeOrgActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class StartEveryThing extends AppCompatActivity {
@@ -18,11 +25,24 @@ public class StartEveryThing extends AppCompatActivity {
 
     private Button Activist;
 
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_every_thing);
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        // Check if the user is already logged in
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            autoLogin(currentUser.getUid());
+        }
+
 
 
         Organization = findViewById(R.id.Organization);
@@ -59,5 +79,30 @@ public class StartEveryThing extends AppCompatActivity {
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
 
     }
+
+    private void autoLogin(String userId) {
+        // Query Firestore to get the user's account type
+        db.collection("teenActivists").document(userId)
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        // User is an activist, redirect to activist home page
+                        startActivity(new Intent(this, HomeActivity.class));
+                        finish();
+                    } else {
+                        // User is an organization, redirect to organization home page
+                        startActivity(new Intent(this, CampaignManagementOrgActivity.class));
+                        finish();
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    // Handle Firestore query failure
+                    Toast.makeText(this, "Error fetching user data!", Toast.LENGTH_SHORT).show();
+                });
+    }
+
+    // ... Implement the login process using Firebase Authentication ...
+
+    // ...
 
 }
--- app/src/main/java/com/example/meet_workshop/homepage/homeorganization/CampaignAnalyticsOrgActivity.java
+++ app/src/main/java/com/example/meet_workshop/homepage/homeorganization/CampaignAnalyticsOrgActivity.java
@@ -1,16 +1,86 @@
 package com.example.meet_workshop.homepage.homeorganization;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.meet_workshop.R;
 
+import java.util.ArrayList;
+
 public class CampaignAnalyticsOrgActivity extends AppCompatActivity {
+
+    private ImageButton profileImageButton;
+    private ImageButton addEventButton;
+    private ImageButton campaignManagementButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campaign_analytics_org);
+
+        profileImageButton = findViewById(R.id.nav_profile);
+        campaignManagementButton = findViewById(R.id.nav_manage);
+        addEventButton = findViewById(R.id.nav_addPost);
+        //searchButton = findViewById(R.id.nav_search);
+
+
+
+
+        ImageButton NavButton = (ImageButton) this.findViewById(R.id.nav_manage);
+        NavButton.setColorFilter(Color.rgb(0,0,0)); // Yellow Tint
+
+
+        profileImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the profileImageButton click event
+                openUserProfile();
+            }
+        });
+
+
+
+        campaignManagementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the profileImageButton click event
+                openCampaignManagement();
+            }
+        });
+
+        addEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the profileImageButton click event
+                openAddEventOrgActivity();
+            }
+        });
     }
+
+    private void openAddEventOrgActivity() {
+        Intent intent = new Intent(this, AddEventOrgActivity.class);
+        startActivity(intent);
+    }
+
+    private void openCampaignManagement() {
+        Intent intent = new Intent(this, CampaignManagementOrgActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
+    private void openUserProfile() {
+        // Start the UserProfile activity
+        Intent intent = new Intent(this, UserProfileOrgActivity.class);
+        startActivity(intent);
+    }
+
 }
